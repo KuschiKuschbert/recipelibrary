62095ece74a90fc95dc15fe0535117704246dcc3
Daniel Kuschmierz <danielkuschmierz@Daniels-Mac-mini.local>
2026-06-30 14:03:40 +1000

    feat(ui): tighten tablet Riviera cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'kuschi-kitchen-v101';
+const CACHE_NAME = 'kuschi-kitchen-v102';
 
 // App shell: HTML pages + core assets (precache on install)
 const SHELL_URLS = [
