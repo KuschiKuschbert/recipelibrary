c64d6238b55bde1e2315e8193276c36640a4b2fa
Daniel Kuschmierz <danielkuschmierz@Daniels-Mac-mini.local>
2026-06-30 23:03:58 +1000

    feat(riviera): polish tablet recipe cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'kuschi-kitchen-v149';
+const CACHE_NAME = 'kuschi-kitchen-v150';
 
 // App shell: HTML pages + core assets (precache on install)
 const SHELL_URLS = [
