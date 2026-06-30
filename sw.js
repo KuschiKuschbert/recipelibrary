b4c735ef882e273ea35da1756dc5ecd1cc982232
Daniel Kuschmierz <danielkuschmierz@Daniels-Mac-mini.local>
2026-06-30 21:06:24 +1000

    feat(ui): align tablet theme cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'kuschi-kitchen-v134';
+const CACHE_NAME = 'kuschi-kitchen-v135';
 
 // App shell: HTML pages + core assets (precache on install)
 const SHELL_URLS = [
