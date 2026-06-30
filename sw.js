8472b4d663caff6536ab26b222de5d6acc984f55
Daniel Kuschmierz <danielkuschmierz@Daniels-Mac-mini.local>
2026-06-30 14:54:14 +1000

    feat(ui): tighten tablet Riviera command area

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'kuschi-kitchen-v107';
+const CACHE_NAME = 'kuschi-kitchen-v108';
 
 // App shell: HTML pages + core assets (precache on install)
 const SHELL_URLS = [
